9fecd283858997a128a2557fc7aa76c8293d0db8
BeshoyMorad <besh0morta@gmail.com>
2021-11-05 19:40:21 +0200

    added the main files of the project

---
+++ admin/includes/temps/footer.php
@@ -0,0 +1,9 @@
+  
+  
+  <div class="footer"></div>
+  <script src="<?php echo $js; ?>jquery-3.6.0.min.js"></script>
+  <script src="<?php echo $js; ?>popper.min.js"></script>
+  <script src="<?php echo $js; ?>bootstrap.min.js"></script>
+  <script src="<?php echo $js; ?>backend.js"></script>
+  </body>
+</html>
