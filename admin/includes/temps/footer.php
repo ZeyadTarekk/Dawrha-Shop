b34bafb4723825eb3fd9dec102d9e70eb8599bf1
BeshoyMorad <besh0morta@gmail.com>
2021-11-05 19:52:09 +0200

    edited some files

--- admin/includes/temps/footer.php
+++ admin/includes/temps/footer.php
@@ -1,7 +1,6 @@
   
   
   <div class="footer"></div>
-  <script src="<?php echo $js; ?>jquery-3.6.0.min.js"></script>
   <script src="<?php echo $js; ?>popper.min.js"></script>
   <script src="<?php echo $js; ?>bootstrap.min.js"></script>
   <script src="<?php echo $js; ?>backend.js"></script>
