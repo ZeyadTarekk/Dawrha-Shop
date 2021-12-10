f1e64c3ec95541a6b6bd966d7a0daf34e1ce087c
Abd-ELrahmanHamza <68310502+Abd-ELrahmanHamza@users.noreply.github.com>
2021-12-11 01:33:20 +0200

    SignIn_include_edit => abdelrahman

--- signin.php
+++ signin.php
@@ -27,9 +27,6 @@ include "admin/includes/temps/header.php";
 
 </div>
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
-        crossorigin="anonymous"></script>
 <script>
     function togglePasswordVisibility() {
         var password = document.getElementById("password");
