0ce07250281e075310f7e901fa0827267621764d
Zeyad Tarek <zeyad.ta01@gmail.com>
2021-12-10 22:20:47 +0200

    Edit Files structure

--- admin/includes/temps/footer.php
+++ admin/includes/temps/footer.php
@@ -3,7 +3,7 @@
   <div class="footer"></div>
   <script src="<?php echo $js; ?>popper.min.js"></script>
   <script src="<?php echo $js; ?>bootstrap.min.js"></script>
-  <script src="<?php echo $js; ?>backend.js"></script>
+  <script src="<?php echo $js; ?>frontend.js"></script>
   </body>
 
   </html>
