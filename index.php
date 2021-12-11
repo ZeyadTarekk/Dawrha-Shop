87a2eade85557a0c9cfb01709dc39acb70034233
Zeyad Tarek <zeyad.ta01@gmail.com>
2021-12-11 12:56:23 +0200

    Navbar fix Notification numer

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php 
-  include "admin/includes/temps/header.php";
-  // include "admin/includes/temps/navbar2(notLoggedInTEMP).php";
-  include "admin/includes/temps/navbar.php";
+  $pageTitle = 'TestNav';
+  include "init.php";
+  // include "includes/temps/navbar.php";
   include "admin/includes/temps/footer.php";
 ?>
