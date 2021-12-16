bcfad30e28ff3e49de3574d86209177d839dded7
Zeyad Tarek <zeyad.ta01@gmail.com>
2021-12-16 21:20:01 +0200

    Remove bootstrap icons link from header

--- includes/temps/header.php
+++ includes/temps/header.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,8 +9,6 @@
   <link rel="stylesheet" href="<?php echo $css?>bootstrap.min.css">
   <link rel="stylesheet" href="<?php echo $css?>all.min.css">
   <link rel="stylesheet" href="<?php echo $css?>frontend.css">
-  <!-- <link rel="stylesheet" href="bootstrap-icons.css"> -->
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
 </head>
 
 <body>
