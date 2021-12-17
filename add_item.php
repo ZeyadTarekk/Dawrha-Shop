6a40a5e158b9876100946d43c22d098e9b6d5d75
Ziad Sherif Muhammed Ibrahim <78238570+ZiadSheriif@users.noreply.github.com>
2021-12-18 00:43:37 +0200

    btn&sizes

--- add_item.php
+++ add_item.php
@@ -22,15 +22,15 @@ include "init.php";
             <div class=" col-lg-5 col-md-12 col-sm-6">
                 <form action="profileSeller.php" method="POST" target=" _self" id="contactFrom"
                     enctype="multipart/form-data">
-                    <div class="mb-4 input-group-lg ">
+                    <div class="mb-4 input-group ">
                         <input type="name" class="form-control " id="namee" placeholder="Item Name" name="name" required
                             autofocus>
                     </div>
-                    <div class="mb-4 input-group-lg">
+                    <div class="mb-4 input-group">
                         <textarea placeholder="Description" class="form-control" id="exampleFormControlTextarea1"
                             name="description" rows="3"></textarea>
                     </div>
-                    <div class="input-group input-group-lg mb-4">
+                    <div class="input-group  mb-4">
                         <select class="form-select input-lg " id="inputGroupSelect02" name="category">
                             <option selected>Choose Categories...</option>
                             <option value="1 ">Plastic</option>
@@ -39,37 +39,37 @@ include "init.php";
                         </select>
                         <label class="input-group-text bg-success text-light" for="inputGroupSelect02">Options</label>
                     </div>
-                    <div class="mb-4 input-group-lg">
+                    <div class="mb-4 input-group">
                         <input type="address" class="form-control" id="address" placeholder="Location Item"
                             name="address" required>
                     </div>
-                    <div class="input-group input-group-lg mb-4">
-                        <input placeholder="Price" name="priceOfItem" type="text" required
-                            class="form-control form-control-lg "
+                    <div class="input-group  mb-4">
+                        <input placeholder="Price" name="priceOfItem" type="text" required class="form-control  "
                             aria-label="Dollar amount (with dot and two decimal places)">
                         <span class="input-group-text bg-success text-light">&#163</span>
                         <span class="input-group-text bg-success text-light">0.00</span>
                     </div>
-                    <div class="input-group input-group-lg  mb-4">
-                        <input placeholder="Discount" max=100 name="discountOfItem" type="number" class="form-control">
+                    <div class="input-group   mb-4">
+                        <input min=0 placeholder="Discount" max=100 name="discountOfItem" type="number"
+                            class="form-control">
                         <span class=" input-group-text  bg-success text-light">&#163</span>
                         <span class="input-group-text bg-success text-light">%</span>
                     </div>
-                    <div class="input-group input-group-lg mb-4 ">
-                        <input name="file" type="file" class="form-control form-control-lg " id="inputGroupFile04"
+                    <div class="input-group  mb-4 ">
+                        <input name="file" type="file" class="form-control " id="inputGroupFile04"
                             aria-describedby="inputGroupFileAddon04 " aria-label="Upload" />
                         <button name="upload-img" class="btn btn-success" type="submit" id="inputGroupFileAddon04"
                             multiple>
                             Upload
                         </button>
                     </div>
-                    <div class="input-group-lg mb-4" id="input_div">
+                    <div class="input-group mb-5" id="input_div">
                         <input required class="form-control " type="number" placeholder="Quantity" name="quantity"
                             size="25" min=0 id=" counting">
                         <!-- <input class=" bg-success text-light" type="button" value="-" id="moins" onclick="minus()">
                         <input class=" bg-success text-light" type="button" value="+" id="pluss" onclick="plus()"> -->
                     </div>
-                    <button class="btn btn-lg btn-success text-align-light mb-4" type="submit" name="done">Add
+                    <button class="btn btn-lg btn-success text-align-light mt-2 mb-4" type="submit" name="done">Add
                         item</button>
                 </form>
             </div>
--- editItem.php
+++ editItem.php
@@ -23,16 +23,16 @@ include "init.php";
             <div class=" col-lg-5 col-md-12">
                 <form action="profileSeller.php" method="POST" target=" _self" id="contactFrom"
                     enctype="multipart/form-data">
-                    <div class="mb-4 input-group-lg ">
+                    <div class="mb-4 input-group ">
                         <input type="name" class="form-control " id="namee" placeholder="Item Name" name="name" required
                             autofocus>
                     </div>
-                    <div class="mb-4 input-group-lg">
+                    <div class="mb-4 input-group">
                         <textarea placeholder="Description" class="form-control" id="exampleFormControlTextarea1"
                             name="description" rows="3"></textarea>
                     </div>
-                    <div class="input-group input-group-lg mb-4">
-                        <select class="form-select input-lg " id="inputGroupSelect02" name="category">
+                    <div class="input-group mb-4">
+                        <select class="form-select  " id="inputGroupSelect02" name="category">
                             <option selected>Choose Categories...</option>
                             <option value="1 ">Plastic</option>
                             <option value="2">Paper</option>
@@ -40,23 +40,23 @@ include "init.php";
                         </select>
                         <label class="input-group-text bg-success text-light" for="inputGroupSelect02">Options</label>
                     </div>
-                    <div class="mb-4 input-group-lg">
+                    <div class="mb-4 input-group">
                         <input type="address" class="form-control" id="address" placeholder="Location Item"
                             name="address" required>
                     </div>
-                    <div class="input-group input-group-lg mb-4">
+                    <div class="input-group  mb-4">
                         <input placeholder="Price" name="priceOfItem" type="text" required class="form-control"
                             aria-label="Dollar amount (with dot and two decimal places)">
                         <span class="input-group-text bg-success text-light">&#163</span>
                         <span class="input-group-text bg-success text-light">0.00</span>
                     </div>
-                    <div class="input-group input-group-lg mb-4">
-                        <input placeholder="Discount" max=100 name="discountOfItem" type="number" required
+                    <div class="input-group  mb-4">
+                        <input placeholder="Discount" min=0 max=100 name="discountOfItem" type="number" required
                             class="form-control">
                         <span class=" input-group-text bg-success text-light">&#163</span>
                         <span class="input-group-text bg-success text-light">%</span>
                     </div>
-                    <div class="input-group input-group-lg mb-4 ">
+                    <div class="input-group  mb-4 ">
                         <input name="file" type="file" class="form-control form-control-lg" id="inputGroupFile04"
                             aria-describedby="inputGroupFileAddon04" aria-label="Upload" />
                         <button name="upload-img" class="btn btn-success" type="submit" id="inputGroupFileAddon04"
@@ -64,13 +64,14 @@ include "init.php";
                             Upload
                         </button>
                     </div>
-                    <div class="input-group-lg mb-4" id="input_div">
+                    <div class="input-group mb-5" id="input_div">
                         <input required class="form-control " type="number" placeholder="Quantity" name="quantity"
                             size="25" min=0 id=" counting">
                         <!-- <input class=" bg-success text-light" type="button" value="-" id="moins" onclick="minus()">
                         <input class=" bg-success text-light" type="button" value="+" id="pluss" onclick="plus()"> -->
                     </div>
-                    <button class="btn btn-lg btn-success text-align-light mb-4" type="submit" name="done">Save
+                    <button class="btn btn-lg btn-success text-align-light mt-2 mb-4" type="submit" name="done">save
+                        item
                     </button>
                 </form>
             </div>
