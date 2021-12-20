cbc44cef076ebc522a36224c3cd35e61d209b50f
Abd-ELrahmanHamza <68310502+Abd-ELrahmanHamza@users.noreply.github.com>
2021-12-20 03:24:45 +0200

    profileBuyer.php

--- profileBuyer.php
+++ profileBuyer.php
@@ -13,7 +13,7 @@ include "init.php";
                 <div class="m-2">
                     <h4 class="d-inline-block">Email: </h4>
                     <a href="mailto:a.m.hamza156@gmail.com" class="mb-2 link-dark fa-1x "><h5
-                                class="text-muted d-inline-block">
+                            class="text-muted d-inline-block">
                             a.m.hamza156@gmail.com
                         </h5></a>
                 </div>
@@ -102,124 +102,19 @@ include "init.php";
             </div>
         </div>
 
-        <!------------------------------------------------>
-        <!--   For sale   -->
-        <!------------------------------------------------>
-        <div class="row justify-content-around" id="forSale">
-            <div class="btn-group" role="group" aria-label="Basic example">
-                <a href="#forSale" class="btn btn-success m-2 rounded-pill btn-lg">Items for sale</a>
-                <a href="#sold" class="btn btn-success m-2 rounded-pill btn-lg">Sold Items</a>
-                <a href="#deleted" class="btn btn-success m-2 rounded-pill btn-lg">Deleted Items</a>
-            </div>
-
-            <div class="row justify-content-between m-3">
-                <div class="col-lg-8 jumbotron m-3">
-                    <div class="container">
-                        <h1 class="display-4">For sale</h1>
-                        <hr class="my-4">
-
-                        <p class="lead">List of all items that are offered for sale.</p>
-                    </div>
-                </div>
-                <div class="col-lg-3 m-0" style="position: sticky; right: 0;">
-                    <a href="add_item.php" class="link-dark">
-                        <div class="card m-md-auto shadow" style="width: 18rem;">
-                            <div class="m-auto">
-                                <i class="bi bi-plus-circle fa-6x"></i>
-                            </div>
-                            <div class="card-body">
-                                <h5 class="card-title text-center">Add Item</h5>
-                            </div>
-                        </div>
-                    </a>
-                </div>
-            </div>
-            <div class="col-sm-12">
-
-                <section class="row flex-row flex-nowrap p-3 overflow-auto profile_scroll rounded position-static " style="gap: 60px;" >
-                    <?php
-                    for ($i = 0;
-                    $i < 10;
-                    $i++) {
-                    echo '
-                    <div class="col-lg-3 m-0 text-center">
-                        <div class="card m-md-auto shadow" style="width: 18rem;">
-                             <a href="#" class="btn btn-danger rounded-pill position-absolute"
-                               style="width: fit-content; top: 0;right: 0">
-                                <span class="badge">0</span></a>
-                                '; ?>
-
-                    <a href="reviewItem.php" style="text-decoration: none;color: black">
-
-                        <img src="<?php echo $imgs . "Login-img.png" ?>" class="card-img-top" alt="Item">
-                        <?php echo '       
-                    <div class="card-body">
-                                <h5 class="card-title">Item Name</h5>
-                                <h6 class="card-title">Category</h6>
-                                <p class="card-text">Some quick example text to build on the card title and make up
-                                    the bulk of the card\'s
-                                    content.</p>
-                                <h4 class="card-title">$30</h4>
-                                <div class="card-body">
-                                    <a href="#" class="btn btn-success">Edit</a>
-                                    <a href="#" class="btn btn-danger">Delete</a>
-                                </div>
-                            </div>
-                            </a>
-                        </div>
-                    </div>
-                    ';
-                        } ?>
-
-                        <div class="col-lg-3 m-0 text-center">
-                            <div class="card m-md-auto shadow" style="width: 18rem;">
-                                <a href="#" class="btn btn-danger rounded-pill position-absolute"
-                                   style="width: fit-content; top: 0;right: 0">
-                                    <span class="badge">0</span></a>
-                                <a href="reviewItem.php" style="text-decoration: none;color: black">
-
-                                    <img src="<?php echo $imgs . "Login-img.png" ?>" class="card-img-top" alt="Item">
-                                    <div class="card-body">
-                                        <h5 class="card-title">Item Name</h5>
-                                        <h6 class="card-title">Category</h6>
-                                        <p class="card-text">Some quick example text to build on the card title and make
-                                            up
-                                            the bulk of the card's
-                                            content.</p>
-                                        <h4 class="card-title">$30</h4>
-                                        <div class="card-body">
-                                            <a href="#" class="btn btn-success">Edit</a>
-                                            <a href="#" class="btn btn-danger">Delete</a>
-                                        </div>
-                                    </div>
-                                </a>
-                            </div>
-                        </div>
-
-<!--                        <div class="col-lg-3 m-0 " style="position: sticky; right: 0;">-->
-<!--                            <a href="add_item.php" class="link-dark">-->
-<!--                                <div class="card m-md-auto shadow" style="width: 18rem;">-->
-<!--                                    <div class="m-auto">-->
-<!--                                        <i class="bi bi-plus-circle fa-9x"></i>-->
-<!--                                    </div>-->
-<!--                                    <div class="card-body">-->
-<!--                                        <h5 class="card-title text-center">Add Item</h5>-->
-<!--                                    </div>-->
-<!--                                </div>-->
-<!--                            </a>-->
-<!--                        </div>-->
-                </section>
-            </div>
-        </div>
 
 
         <!------------------------------------------------>
         <!--   Sold  out  -->
         <!------------------------------------------------>
-        <div class="row justify-content-around" id="sold">
+        <div class="row justify-content-around" id="ordered">
+            <div class="btn-group" role="group" aria-label="Basic example">
+                <a href="#ordered" class="btn btn-success m-2 rounded-pill btn-lg">Sold Items</a>
+                <a href="#deleted" class="btn btn-success m-2 rounded-pill btn-lg">Deleted Items</a>
+            </div>
             <div class="jumbotron jumbotron-fluid m-3">
                 <div class="container">
-                    <h1 class="display-4">Sold Out</h1>
+                    <h1 class="display-4">Ordered Items</h1>
                     <hr class="my-4">
                     <p class="lead">List of all Sold items.</p>
                 </div>
@@ -234,7 +129,7 @@ include "init.php";
                     <div class="col-lg-3 m-0 text-center">
                         <div class="card m-md-auto shadow" style="width: 18rem;">
                                 '; ?>
-                    <a href="reviewItem.php" style="text-decoration: none;color: black;filter:grayscale(100%)">
+                    <a href="reviewItem.php" style="text-decoration: none;color: black;">
 
                         <img src="<?php echo $imgs . "Login-img.png" ?>" class="card-img-top" alt="Item">
                         <?php echo '       
