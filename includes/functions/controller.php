24251713ecbf355b8a262bfd284ad7ea730efe04
Zeyad Tarek <zeyad.ta01@gmail.com>
2021-12-21 02:05:04 +0200

    Items and categories from database

--- includes/functions/controller.php
+++ includes/functions/controller.php
@@ -1,7 +1,23 @@
 <?php 
 //this file will be used to write sql commands like insert a new buyer or select items or etc..
-
-
+function getItems($db) {
+  $sql = "SELECT * FROM item";
+  $stmt = $db->query($sql);
+  $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+  return $result;
+}
+function getItemsImages($db) {
+  $sql = "SELECT * FROM itemimage";
+  $stmt = $db->query($sql);
+  $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+  return $result;
+}
+function getCategories($db) {
+  $sql = "SELECT * FROM category";
+  $stmt = $db->query($sql);
+  $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+  return $result;
+}
 
 
 
--- index.php
+++ index.php
@@ -1,6 +1,30 @@
 <?php
-$pageTitle = 'Home Page';
-include "init.php";
+  $pageTitle = 'Home Page';
+  include "init.php";
+  $items = getItems($db);
+  $categories = getCategories($db);
+  $itemImages = getItemsImages($db);
+
+  // Linking item cateogry to each item
+    for($i=0,$j=count($categories); $i < $j ; ++$i) {
+      for($k=0,$l=count($items);$k < $l;++$k){
+        if($items[$k]['categoryId']==$categories[$i]['cateogryId']){
+          $items[$k]["categoryName"]=$categories[$i]['categoryName'];
+          // echo $items[$k]["categoryName"];
+          } 
+        }
+    }
+
+    // linking items image to each item
+    for($i=0,$j=count($itemImages); $i < $j ; ++$i) {
+      for($k=0,$l=count($items);$k < $l;++$k){
+        if($items[$k]['itemId']==$itemImages[$i]['itemId']){
+          $items[$k]["image"]=$itemImages[$i]['image'];
+          // echo $items[$k]["image"];
+          } 
+        }
+    }
+
 ?>
 
 <div class="main-page pt-5 pb-5 bg-light">
@@ -9,25 +33,23 @@ include "init.php";
       <ol class="item-categories ">
         <li class=""><button class="btn btn-success active"><a href="#">All</a></button>
         </li>
-        <li><button class="btn btn-success"><a href="#">Category1</a></button></li>
-        <li><button class="btn btn-success"><a href="#">Category2</a></button></li>
-        <li><button class="btn btn-success"><a href="#">Category3</a></button></li>
-        <li><button class="btn btn-success"><a href="#">Category4</a></button></li>
+        <?php foreach($categories as $cat): ?>
+        <li><button class="btn btn-success"><a href="#"><?php echo $cat['categoryName'] ?></a></button></li>
+        <?php endforeach; ?>
       </ol>
     </div>
     <div class="text-center">
       <div class="row row-of-card g-5 justify-content-center align-items-center">
+        <?php foreach($items as $ite): ?>
         <div class="col-8 col-lg-4 col-xl-3 ">
           <a href="reviewitem.php" style="text-decoration:none; color:black">
             <div class="card m-md-auto shadow" style="width: 18rem;">
-              <img src="<?php echo $imgs . "Login-img.png" ?>" class="card-img-top" alt="Item">
+              <img src="<?php echo $imgs . $ite['image'] ?>" class="card-img-top" alt="Item">
               <div class="card-body">
-                <h5 class="card-title">Item Name</h5>
-                <h6 class="card-title">Category</h6>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-                  card's
-                  content.</p>
-                <h4 class="card-title">$30</h4>
+                <h5 class="card-title"><?php echo $ite['title'] ?></h5>
+                <h6 class="card-title"><?php echo $ite['categoryName'] ?></h6>
+                <p class="card-text"> <?php echo $ite['description'] ?></p>
+                <h4 class="card-title"><?php echo $ite['price'] . '$' ?></h4>
                 <div class="card-body">
                   <a href="reviewitem.php" class="btn btn-success">Review</a>
                 </div>
@@ -35,208 +57,7 @@ include "init.php";
             </div>
           </a>
         </div>
-        <div class="col-8 col-lg-4 col-xl-3 ">
-          <a href="reviewitem.php" style="text-decoration:none; color:black">
-            <div class="card m-md-auto shadow" style="width: 18rem;">
-              <img src="<?php echo $dataimages . "image-product-1.jpg" ?>" class="card-img-top" alt="Item">
-              <div class="card-body">
-                <h5 class="card-title">Item Name</h5>
-                <h6 class="card-title">Category</h6>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-                  card's
-                  content.</p>
-                <h4 class="card-title">$30</h4>
-                <div class="card-body">
-                  <a href="reviewitem.php" class="btn btn-success">Review</a>
-                </div>
-              </div>
-            </div>
-          </a>
-        </div>
-        <div class="col-8 col-lg-4 col-xl-3 ">
-          <a href="reviewitem.php" style="text-decoration:none; color:black">
-            <div class="card m-md-auto shadow" style="width: 18rem;">
-              <img src="<?php echo $dataimages . "image-product-2.jpg" ?>" class="card-img-top" alt="Item">
-              <div class="card-body">
-                <h5 class="card-title">Item Name</h5>
-                <h6 class="card-title">Category</h6>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-                  card's
-                  content.</p>
-                <h4 class="card-title">$30</h4>
-                <div class="card-body">
-                  <a href="reviewitem.php" class="btn btn-success">Review</a>
-                </div>
-              </div>
-            </div>
-          </a>
-        </div>
-        <div class="col-8 col-lg-4 col-xl-3 ">
-          <a href="reviewitem.php" style="text-decoration:none; color:black">
-            <div class="card m-md-auto shadow" style="width: 18rem;">
-              <img src="<?php echo $dataimages . "image-product-3.jpg" ?>" class="card-img-top" alt="Item">
-              <div class="card-body">
-                <h5 class="card-title">Item Name</h5>
-                <h6 class="card-title">Category</h6>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-                  card's
-                  content.</p>
-                <h4 class="card-title">$30</h4>
-                <div class="card-body">
-                  <a href="reviewitem.php" class="btn btn-success">Review</a>
-                </div>
-              </div>
-            </div>
-          </a>
-        </div>
-        <div class="col-8 col-lg-4 col-xl-3 ">
-          <a href="reviewitem.php" style="text-decoration:none; color:black">
-            <div class="card m-md-auto shadow" style="width: 18rem;">
-              <img src="<?php echo $dataimages . "image-product-4.jpg" ?>" class="card-img-top" alt="Item">
-              <div class="card-body">
-                <h5 class="card-title">Item Name</h5>
-                <h6 class="card-title">Category</h6>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-                  card's
-                  content.</p>
-                <h4 class="card-title">$30</h4>
-                <div class="card-body">
-                  <a href="reviewitem.php" class="btn btn-success">Review</a>
-                </div>
-              </div>
-            </div>
-          </a>
-        </div>
-        <div class="col-8 col-lg-4 col-xl-3 ">
-          <a href="reviewitem.php" style="text-decoration:none; color:black">
-            <div class="card m-md-auto shadow" style="width: 18rem;">
-              <img src="<?php echo $imgs . "Signup.png" ?>" class="card-img-top" alt="Item">
-              <div class="card-body">
-                <h5 class="card-title">Item Name</h5>
-                <h6 class="card-title">Category</h6>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-                  card's
-                  content.</p>
-                <h4 class="card-title">$30</h4>
-                <div class="card-body">
-                  <a href="reviewitem.php" class="btn btn-success">Review</a>
-                </div>
-              </div>
-            </div>
-          </a>
-        </div>
-
-        <div class="col-8 col-lg-4 col-xl-3 ">
-          <a href="reviewitem.php" style="text-decoration:none; color:black">
-            <div class="card m-md-auto shadow" style="width: 18rem;">
-              <img src="<?php echo $imgs . "Login-img.png" ?>" class="card-img-top" alt="Item">
-              <div class="card-body">
-                <h5 class="card-title">Item Name</h5>
-                <h6 class="card-title">Category</h6>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-                  card's
-                  content.</p>
-                <h4 class="card-title">$30</h4>
-                <div class="card-body">
-                  <a href="reviewitem.php" class="btn btn-success">Review</a>
-                </div>
-              </div>
-            </div>
-          </a>
-        </div>
-        <div class="col-8 col-lg-4 col-xl-3 ">
-          <a href="reviewitem.php" style="text-decoration:none; color:black">
-            <div class="card m-md-auto shadow" style="width: 18rem;">
-              <img src="<?php echo $dataimages . "image-product-1.jpg" ?>" class="card-img-top" alt="Item">
-              <div class="card-body">
-                <h5 class="card-title">Item Name</h5>
-                <h6 class="card-title">Category</h6>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-                  card's
-                  content.</p>
-                <h4 class="card-title">$30</h4>
-                <div class="card-body">
-                  <a href="reviewitem.php" class="btn btn-success">Review</a>
-                </div>
-              </div>
-            </div>
-          </a>
-        </div>
-        <div class="col-8 col-lg-4 col-xl-3 ">
-          <a href="reviewitem.php" style="text-decoration:none; color:black">
-            <div class="card m-md-auto shadow" style="width: 18rem;">
-              <img src="<?php echo $dataimages . "image-product-2.jpg" ?>" class="card-img-top" alt="Item">
-              <div class="card-body">
-                <h5 class="card-title">Item Name</h5>
-                <h6 class="card-title">Category</h6>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-                  card's
-                  content.</p>
-                <h4 class="card-title">$30</h4>
-                <div class="card-body">
-                  <a href="reviewitem.php" class="btn btn-success">Review</a>
-                </div>
-              </div>
-            </div>
-          </a>
-        </div>
-        <div class="col-8 col-lg-4 col-xl-3 ">
-          <a href="reviewitem.php" style="text-decoration:none; color:black">
-            <div class="card m-md-auto shadow" style="width: 18rem;">
-              <img src="<?php echo $dataimages . "image-product-3.jpg" ?>" class="card-img-top" alt="Item">
-              <div class="card-body">
-                <h5 class="card-title">Item Name</h5>
-                <h6 class="card-title">Category</h6>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-                  card's
-                  content.</p>
-                <h4 class="card-title">$30</h4>
-                <div class="card-body">
-                  <a href="reviewitem.php" class="btn btn-success">Review</a>
-                </div>
-              </div>
-            </div>
-          </a>
-        </div>
-        <div class="col-8 col-lg-4 col-xl-3 ">
-          <a href="reviewitem.php" style="text-decoration:none; color:black">
-            <div class="card m-md-auto shadow" style="width: 18rem;">
-              <img src="<?php echo $dataimages . "image-product-4.jpg" ?>" class="card-img-top" alt="Item">
-              <div class="card-body">
-                <h5 class="card-title">Item Name</h5>
-                <h6 class="card-title">Category</h6>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-                  card's
-                  content.</p>
-                <h4 class="card-title">$30</h4>
-                <div class="card-body">
-                  <a href="reviewitem.php" class="btn btn-success">Review</a>
-                </div>
-              </div>
-            </div>
-          </a>
-        </div>
-        <div class="col-8 col-lg-4 col-xl-3 ">
-          <a href="reviewitem.php" style="text-decoration:none; color:black">
-            <div class="card m-md-auto shadow" style="width: 18rem;">
-              <img src="<?php echo $imgs . "Signup.png" ?>" class="card-img-top" alt="Item">
-              <div class="card-body">
-                <h5 class="card-title">Item Name</h5>
-                <h6 class="card-title">Category</h6>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-                  card's
-                  content.</p>
-                <h4 class="card-title">$30</h4>
-                <div class="card-body">
-                  <a href="reviewitem.php" class="btn btn-success">Review</a>
-                </div>
-              </div>
-            </div>
-          </a>
-        </div>
-
-
-
+        <?php endforeach; ?>
       </div>
     </div>
   </div>
