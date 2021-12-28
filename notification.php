81b76697d5551b6bd31b6432b33c8e53cfde55d0
Zeyad Tarek <zeyad.ta01@gmail.com>
2021-12-28 02:51:29 +0200

    Notification page

---
+++ notification.php
@@ -0,0 +1,112 @@
+<?php
+$pageTitle = 'Notifications';
+include "init.php";
+?>
+
+<div class="container pt-5 ">
+  <div class="row  justify-content-center">
+    <div class="col-lg-9">
+      <div class="box shadow-sm rounded bg-white mb-3">
+        <div class="box-title border-bottom p-3">
+          <h6 class="m-0">Recent</h6>
+        </div>
+        <div class="box-body p-0">
+          <div class="
+                  p-3
+                  d-flex
+                  justify-content-between
+                  align-items-center
+                  bg-light
+                  border-bottom
+                ">
+            <div class="fw-bold mr-3">
+              <div>Notification Header</div>
+              <div class="small">
+                MessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessage
+              </div>
+            </div>
+            <div class="text-right text-muted pt-1">3d</div>
+          </div>
+          <div class="
+                  p-3
+                  d-flex
+                  justify-content-between
+                  align-items-center
+                  bg-light
+                  border-bottom
+                ">
+            <div class="fw-bold mr-3">
+              <div>Notification Header</div>
+              <div class="small">
+                MessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessage
+              </div>
+            </div>
+            <div class="text-right text-muted pt-1">3d</div>
+          </div>
+          <div class="
+                  p-3
+                  d-flex
+                  justify-content-between
+                  align-items-center
+                  bg-light
+                  border-bottom
+                ">
+            <div class="fw-bold mr-3">
+              <div>Notification Header</div>
+              <div class="small">
+                MessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessage
+              </div>
+            </div>
+            <div class="text-right text-muted pt-1">3d</div>
+          </div>
+
+
+        </div>
+      </div>
+      <div class="box shadow-sm rounded bg-white mb-3">
+        <div class="box-title border-bottom p-3">
+          <h6 class="m-0">Earlier</h6>
+        </div>
+        <div class="box-body p-0">
+          <div class="
+                  p-3
+                  d-flex
+                  justify-content-between
+                  align-items-center
+                  bg-white
+                  border-bottom
+                ">
+            <div class="fw-normal mr-3">
+              <div>Notification Header</div>
+              <div class="small">
+                MessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessage
+              </div>
+            </div>
+            <div class="text-right text-muted pt-1">3d</div>
+          </div>
+          <div class="
+                  p-3
+                  d-flex
+                  justify-content-between
+                  align-items-center
+                  bg-white
+                  border-bottom
+                ">
+            <div class="fw-normal mr-3">
+              <div>Notification Header</div>
+              <div class="small">
+                MessageMessageMessageMessageMessageMessageMessageMessageMessageMessageMessage
+              </div>
+            </div>
+            <div class="text-right text-muted pt-1">3d</div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+<?php include $tpl . "footer.php" ?>
