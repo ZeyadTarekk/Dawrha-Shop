6a42fef367ef27bba5a1a0be59711af8671cbbb3
Ziad Sherif Muhammed Ibrahim <78238570+ZiadSheriif@users.noreply.github.com>
2022-01-02 07:43:00 +0200

    editItem with replace  header with script

--- editItem.php
+++ editItem.php
@@ -3,111 +3,128 @@ $pageTitle = 'Edit Item';
 include "init.php";
 
 if(!isset($_SESSION['username'])){
-    header("Location: signin.php");
-    return;
+ ?>
+<script>
+window.location.href = "signup.php";
+</script>
+<?php
 }
 if(isset($_GET['id'])){
-    $_SESSION['itemID']=$_GET['id'];
+    $_SESSION['itemID']=$_GET['id'];}
+ 
+
+
+    if(isset($_POST['DONE']))
+    {
+            //filter data
+        $_SESSION["item_name"]=input_data($_POST['name']);
+        $_SESSION["price"]=input_data($_POST['priceOfItem']);
+        $_SESSION["discount_item"]=input_data($_POST['discountOfItem']);
+        $_SESSION["quantity_item"]=input_data( $_POST['quantity']);
+        $_SESSION["description_item"]=input_data( $_POST['description']);
+        $_SESSION["filepath"]=input_data( basename($_FILES['file']['name']));
+        $_SESSION["city"]=input_data($_POST['city']);
+        $_SESSION["country"]=input_data( $_POST['country']);
+        $_SESSION["categoryId"]=input_data($_POST['category']);
+        $_SESSION['homeNum']=input_data($_POST['homenumber']);
+        $_SESSION['st']=input_data($_POST['street']);
+        $_SESSION['st_er']="";
+        $_SESSION["pricerr"] = "";
+        $_SESSION["cat_er"]="";
+        $_SESSION["city_er"]="";
+        $_SESSION["country_er"]="";
+        $_SESSION['DB_er']="";
+
+        if( $_SESSION["item_name"]!=""||  $_SESSION["price"]!=""
+        || $_SESSION["discount_item"]!=""||$_SESSION["quantity_item"]!=""
+        || $_SESSION["description_item"]!=""||$_SESSION["filepath"]!=""||
+        $_SESSION["city"]!=""||  $_SESSION["categoryId"]!="Choose Categories..."||$_SESSION["country"]!=""||
+        $_SESSION["st"]!=""|| $_SESSION['homeNum']!=""){
+
+
+
+
+
+    if($_SESSION['item_name']!=""){
+    updateTitle($db,$_SESSION['itemID'],$_SESSION['item_name']);
+    $_SESSION['DB_er']=1;
     }
-    else{
-        header("Location: signin.php");
-        return;    
+    if($_SESSION['price']!=""){
+        if(ctype_digit($_SESSION["price"]) &&$_SESSION["price"]<0){
+            updatePrice($db,$_SESSION['itemID'],$_SESSION['price']);
+        }
+        else{
+            $_SESSION["pricerr"]="* Only Numeric Positive Value is Allowed";
+        }
     }
-    
 
-
-if(isset($_POST['DONE']))
-{ 
-        //filter data
-$_SESSION["item_name"]=input_data($_POST['name']);
-$_SESSION["price"]=input_data($_POST['priceOfItem']);
-$_SESSION["disocunt_item"]=input_data($_POST['discountOfItem']);
-$_SESSION["quantity_item"]=input_data( $_POST['quantity']);
-$_SESSION["description_item"]=input_data( $_POST['description']);
-$_SESSION["filepath"]=input_data( basename($_FILES['file']['name']));
-$_SESSION["city"]=input_data($_POST['city']);
-$_SESSION["country"]=input_data( $_POST['country']);
-$_SESSION["category_item"]=input_data($_POST['category']);
-$_SESSION['homeNum']=input_data($_POST['homenumber']);
-$_SESSION['st']=input_data($_POST['street']);
-$_SESSION['st_er']="";
-$_SESSION["pricerr"] = "";
-$_SESSION["cat_er"]="";
-$_SESSION["city_er"]="";
-$_SESSION["country_er"]="";
-$_SESSION['DB_er']="";
-
-//validate priceItem
-// if(!ctype_digit($_SESSION["price"]) &&$_SESSION["price"]<0){
-//     $_SESSION["pricerr"]="* Only Numeric Positive Value is Allowed";
-// }
-
-                //street validation
-// if(!ctype_alpha($_SESSION["st"])){
-//     $_SESSION["st_er"]="* Only Alphabets and White Space Are Allowed";
-// }    
-
-    
-
-        //validate city & country
-// if((!ctype_alpha( $_SESSION["city"]))||(!ctype_alpha($_SESSION["country"]))){
-//     $_SESSION["country_er"]="* Only Alphabets and White Space Are Allowed";
-// }
-
-        //validate Category
-// if($_SESSION["category_item"]=="Choose Categories..."){
-//     $_SESSION["cat_er"]=" * Please Choose Category";
-//     }
-
-if($_SESSION['item_name']!=""){
-updateTitle($db,$_SESSION['itemID'],$_SESSION['item_name']);
-$_SESSION['DB_er']=1;
-;}
-if($_SESSION['price']!=""){
-    if(!ctype_digit($_SESSION["price"]) &&$_SESSION["price"]<0){
-        $_SESSION["pricerr"]="* Only Numeric Positive Value is Allowed";
-    }
-updatePrice($db,$_SESSION['itemID'],$_SESSION['price']);}
 if(isset($_SESSION['description_item'])&&$_SESSION['description_item']!=""){
 updateDescription($db,$_SESSION['itemID'],$_SESSION['description_item']);}
-if(isset($_SESSION['country'])&&$_SESSION['country']!=""){
-if(ctype_alpha($_SESSION['country'])){
-updateCountry($db,$_SESSION['itemID'],$_SESSION['country']);}
-else{
-$_SESSION["country_er"]="* Only Alphabets and White Space Are Allowed";}
-}
-if(isset($_SESSION['city'])&&$_SESSION['city']!=""){
-    if(ctype_alpha( $_SESSION["city"])){
-    updateCity($db,$_SESSION['itemID'],$_SESSION['city']);}
+
+
+    if(isset($_SESSION['country'])&&$_SESSION['country']!=""){
+        
+    if(ctype_alpha($_SESSION['country'])){
+    updateCountry($db,$_SESSION['itemID'],$_SESSION['country']);}
+    
     else{
-        $_SESSION["country_er"]="* Only Alphabets and White Space Are Allowed";}
+    $_SESSION["country_er"]="* Only Alphabets and White Space Are Allowed";}
     }
-if(isset($_SESSION['st'])&&$_SESSION['st']!=""){
-    if(!ctype_alpha($_SESSION["st"])){
-updateStreet($db,$_SESSION['itemID'],$_SESSION['st']);}
-else{
-$_SESSION["st_er"]="* Only Alphabets and White Space Are Allowed";
-}
-}
-if($_SESSION["category_item"]!="Choose Categories..."){
-updateCategory($db,$_SESSION['itemID'],$_SESSION['category_item']);}
-if(isset($_SESSION['quantity'])&&$_SESSION['quantity']!=""){
-updateQuantity($db,$_SESSION['itemID'],$_SESSION['quantity_item']);}
-if($_SESSION['disocunt_item']!=0){
-updateDiscount($db,$_SESSION['itemID'],$_SESSION['disocunt_item']);
-}
-            
-    $targetDir = "uploads/";
-    $targetFilePath = $targetDir . $_SESSION["filepath"];
-    $fileType = pathinfo($targetFilePath,PATHINFO_EXTENSION);
-        $allowTypes = array('jpg','png','jpeg','gif','pdf');
-        if(in_array($fileType, $allowTypes))
-        {
-            if(move_uploaded_file($_FILES["file"]["tmp_name"], $_SESSION["filepath"])){
-                    insertImage($_SESSION["filepath"],$db);
-                }
+
+    
+    if(isset($_SESSION['city'])&&$_SESSION['city']!=""){
+        if(ctype_alpha( $_SESSION["city"])){
+        updateCity($db,$_SESSION['itemID'],$_SESSION['city']);}
+        else{
+            $_SESSION["country_er"]="* Only Alphabets and White Space Are Allowed";}
         }
+
+
+    if(isset($_SESSION['st'])&&$_SESSION['st']!=""){
+        
+        if(ctype_alpha($_SESSION["st"])){
+    updateStreet($db,$_SESSION['itemID'],$_SESSION['st']);}
+    else{
+    $_SESSION["st_er"]="* Only Alphabets and White Space Are Allowed";
+    }
+    }
+
+    
+    if($_SESSION["categoryId"]!="Choose Categories..."){
+    updateCategory($db,$_SESSION['itemID'],$_SESSION['categoryId']);}
+
+
+    
+    if(isset($_SESSION['quantity'])&&$_SESSION['quantity']!=""){
+    updateQuantity($db,$_SESSION['itemID'],$_SESSION['quantity_item']);}
+
+    
+    if($_SESSION['discount_item']!=""){
+    updateDiscount($db,$_SESSION['itemID'],$_SESSION['discount_item']);
+    }
+                
+        $targetDir = "uploads/";
+        $targetFilePath = $targetDir . $_SESSION["filepath"];
+        $fileType = pathinfo($targetFilePath,PATHINFO_EXTENSION);
+            $allowTypes = array('jpg','png','jpeg','gif','pdf');
+            if(in_array($fileType, $allowTypes))
+            {
+                if(move_uploaded_file($_FILES["file"]["tmp_name"], $_SESSION["filepath"])){
+                        insertImage($_SESSION["filepath"],$db);
+                    }
+            }
+    }
+else{
+    $_SESSION['DB_er'] = "wrong";
+?>
+<script>
+window.location.href = "editItem.php";
+</script>
+
+<?php
+return;
 }
+} 
 ?>
 <div class="container-fluid ">
     <div class=" row justify-content-center  ">
@@ -136,9 +153,9 @@ updateDiscount($db,$_SESSION['itemID'],$_SESSION['disocunt_item']);
                         } ;?>" rows="2"></textarea>
                     </div>
                     <div class="input-group  mb-4">
-                        <select value="<?php if(isset($_SESSION["category_item"])){
-                                echo $_SESSION["category_item"]; 
-                                unset($_SESSION["category_item'"]);
+                        <select value="<?php if(isset($_SESSION["categoryId"])){
+                                echo $_SESSION["categoryId"]; 
+                                unset($_SESSION["categoryId'"]);
                         }?>" class="form-select " id="inputGroupSelect02" name="category">
                             <option selected> Choose Categories...</option>
                             <?php  $row = getCategories($db);
@@ -204,9 +221,9 @@ updateDiscount($db,$_SESSION['itemID'],$_SESSION['disocunt_item']);
                 } ?></p>
                     <div class=" input-group mb-4">
                         <input min=0 placeholder="Discount" max=100 name="discountOfItem" type="number"
-                            class="form-control" value="<?php if(isset($_SESSION["disocunt_item"])){
-                                echo $_SESSION["disocunt_item"]; 
-                                unset($_SESSION["disocunt_item"]);}?>">
+                            class="form-control" value="<?php if(isset($_SESSION["discount_item"])){
+                                echo $_SESSION["discount_item"]; 
+                                unset($_SESSION["discount_item"]);}?>">
                         <span class=" input-group-text  bg-success text-light">&#163</span>
                         <span class="input-group-text bg-success text-light">%</span>
                     </div>
@@ -232,14 +249,9 @@ updateDiscount($db,$_SESSION['itemID'],$_SESSION['disocunt_item']);
                 <img src=" layout/images/editing.png" alt=" item's photo" class="img-fluid">
             </div>
             <?php 
-             if (isset($_SESSION['DB_er'])&&$_SESSION['DB_er']==1) {
-             echo '<div class="alert alert-success w-50" role="alert" >Successfully Submitted
-                      <i class="bi bi-check"></i></div>';
-                     unset($_SESSION["DB_er"]);}
-                 else{
-                     if(isset($_SESSION['DB_er'])){
-                    echo '<div class="alert alert-danger w-50" role="alert">Invalid Submit !</div>'; 
-                    unset($_SESSION["DB_er"]);}
+                     if(isset($_SESSION['DB_er'])&&$_SESSION['DB_er']=="wrong"){
+                    echo '<div class="alert alert-danger w-50" role="alert">Item is Not Edited</div>'; 
+                    unset($_SESSION["DB_er"]);
                 }?>
         </div>
     </div>
